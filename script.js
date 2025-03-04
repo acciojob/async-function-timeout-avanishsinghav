26497e5d6c88b152ca8c9ade592ea1ced22cd67f
Avanish Singh <107432071+avanishsinghav@users.noreply.github.com>
2025-03-04 08:41:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+async function displayMessageAfterDelay(message, delay){
+	await new Promise(resolve => setTimeout(resolve,delay));
+	document.getElementById('output').innerText = message;
+}
+
+
+const btn = document.getElementById('btn');
+const textinput = document.getElementById('text');
+const delayInput = document.getElementById('delay');
+
+btn.addEventListener('click',function(){
+	const message = textinput.value;
+	const delay = parseInt(delayInput.value);
+
+	if(message && !isNaN(delay) && delay >= 0){
+		displayMessageAfterDelay(message,delay);
+	}
+	else{
+		document.getElementById('output').innerText = "please enter a valid message and delay."
+	}
+})
